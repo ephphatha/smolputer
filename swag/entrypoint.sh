b7df6eb382c68bfb183274484add76eb964c53a7
ephphatha <ephphatha@thelettereph.com>
2023-01-01 19:04:37 +1100

    try running the dns config script as an s6 oneshot?

--- swag/entrypoint.sh
+++ swag/entrypoint.sh
@@ -1,6 +1,4 @@
-#! /bin/sh
+#! /bin/sh -e
 if [ -n "$CLOUDFLARE_API_TOKEN_FILE" ]; then
     echo "dns_cloudflare_api_token = `cat $CLOUDFLARE_API_TOKEN_FILE`" > /config/dns-conf/cloudflare.ini
 fi
-
-exec "$@"
