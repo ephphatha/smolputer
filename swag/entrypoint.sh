9bd266abeae38b4433da2aceac5ad20eb283860e
ephphatha <ephphatha@thelettereph.com>
2023-01-01 18:02:26 +1100

    fix entrypoint

--- swag/entrypoint.sh
+++ swag/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
-if [ -n $CLOUDFLARE_API_TOKEN_FILE ] then
+if [ -n "$CLOUDFLARE_API_TOKEN_FILE" ]; then
     echo "dns_cloudflare_api_token = `cat $CLOUDFLARE_API_TOKEN_FILE`" > /config/dns-conf/cloudflare.ini
 fi
+
+exec "$@"
